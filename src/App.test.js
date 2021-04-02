38bbf9ea7d5868f775f52cf5900a1bd7c34b8d35
Sudhirsin <lion.sud.k@gmail.com>
2021-04-03 00:00:33 +0530

    cofigure ci/cd

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/Sudhir Singh/i);
+  expect(h1Element).toBeInTheDocument();
 });
